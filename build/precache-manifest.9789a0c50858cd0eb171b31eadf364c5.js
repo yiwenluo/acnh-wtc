35adc2fa4e66812bb94c1181f41459427fa5fe74
yiwenluo <louis.yiwenluo@gmail.com>
2020-05-22 15:48:50 -0400

    Updated .gitignore to not ignore build/ and added build/

---
+++ build/precache-manifest.9789a0c50858cd0eb171b31eadf364c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229bacdc8f15de0a69894f1b223dad0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97e721d83ccc9a2b3722",
+    "url": "/static/css/main.c50b0c12.chunk.css"
+  },
+  {
+    "revision": "935c24ad36b53f33c69e",
+    "url": "/static/js/2.d388351a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d388351a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e721d83ccc9a2b3722",
+    "url": "/static/js/main.3bd6bd0e.chunk.js"
+  },
+  {
+    "revision": "9ac92641162392807475",
+    "url": "/static/js/runtime-main.a096dfe3.js"
+  }
+]);
