36ca87d4ee15dc1db304a23de55224ed4379c924
yiwenluo <louis.yiwenluo@gmail.com>
2020-05-31 18:02:17 -0400

    Updates

---
+++ precache-manifest.47076660f4556e19248287d594c70d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2115e621172e90c106d38188cc0e4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e139e4491f18fdaf5c",
+    "url": "/static/css/main.374d1209.chunk.css"
+  },
+  {
+    "revision": "f841e7af5b0ac282953b",
+    "url": "/static/js/2.156ee0f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.156ee0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e139e4491f18fdaf5c",
+    "url": "/static/js/main.5e8d636e.chunk.js"
+  },
+  {
+    "revision": "9ac92641162392807475",
+    "url": "/static/js/runtime-main.a096dfe3.js"
+  }
+]);
