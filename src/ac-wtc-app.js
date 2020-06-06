62fb3aaeeee62729673cc5f6718ac5e952600e5f
yiwenluo <louis.yiwenluo@gmail.com>
2020-06-06 14:33:20 -0400

    Added ac-wtc-app component

---
+++ src/ac-wtc-app.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+
+import {AcCards} from './ac-cards.js'
+import './ac-wtc-app.css';
+
+function getHemisphere() {
+  // TODO: implement the real check usinggeolocation APIs. 
+  return 'N';
+}
+
+export class AcnhWtcApp extends React.Component {
+
+  filterNow(list) {
+    const now = new Date();
+    const hour = now.getHours();
+    const isNorthernHemi = getHemisphere() !== 'S';
+    const month = now.getMonth() + 1;
+
+    return list.filter((item) => {
+      return item.available(isNorthernHemi, month, hour);
+    });
+  }
+
+  render() {
+    const bugs = this.filterNow(this.props.data.bugs);
+    const fish = this.filterNow(this.props.data.fish);
+    
+    return (
+      <div>
+        <h2 className="header">ACNH - What To Catch</h2>
+        <AcCards bugs={bugs} fish={fish}/>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,53 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {DataStore} from './data-store.js';
 // import {AcControls} from './ac-controls.js'
-import {AcCards} from './ac-cards.js'
+import {AcnhWtcApp} from './ac-wtc-app.js'
 import './index.css';
 
-class AcHead extends React.Component {
-  render() {
-    return (
-      <h2 className="header">ACNH - What To Catch</h2>
-    );
-  }
-}
-
-class AcnhWTC extends React.Component {
-
-  filterNow(list) {
-    const now = new Date();
-    const hour = now.getHours();
-    const isNorthernHemi = getHemisphere() !== 'S';
-    const month = now.getMonth() + 1;
-
-    return list.filter((item) => {
-      return item.available(isNorthernHemi, month, hour);
-    });
-  }
-
-  render() {
-    const bugs = this.filterNow(this.props.data.bugs);
-    const fish = this.filterNow(this.props.data.fish);
-    
-    return (
-      <div>
-        <AcHead />
-        <AcCards bugs={bugs} fish={fish}/>
-      </div>
-    );
-  }
-}
-
 const data = new DataStore();
-
-function getHemisphere() {
-  // TODO: implement the real check usinggeolocation APIs. 
-  return 'N';
-}
 
 // ========================================
 
 ReactDOM.render(
-  <AcnhWTC data={data}/>,
+  <AcnhWtcApp data={data}/>,
   document.getElementById('root')
 );
