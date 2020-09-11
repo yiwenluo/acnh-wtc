b4e889d0895f5c97ac6d8a08df3269c6b61a4268
Wan Sze Cheng <vincy.wscheng@gmail.com>
2020-09-10 21:21:59 -0400

    Updated header to be an svg

--- src/ac-wtc-app.js
+++ src/ac-wtc-app.js
@@ -4,7 +4,8 @@ import React from 'react';
 import AcCards from './ac-cards.js'
 import AcFab from './ac-fab.js'
 import './ac-wtc-app.css';
-import header from './assets/header.png';
+// import header from './assets/header.svg';
+import { ReactComponent as Header } from './assets/header.svg';
 
 export class AcnhWtcApp extends React.Component {
 
@@ -12,7 +13,7 @@ export class AcnhWtcApp extends React.Component {
     
     return (
       <div class="app-container">
-        <img className="header" src={header}></img>
+        <Header className="header"/>
         <AcCards/>
         <AcFab />
       </div>
