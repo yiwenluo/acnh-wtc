dbd61f10123d10ab1b20464a93846682add292ae
Wan Sze Cheng <vincy.wscheng@gmail.com>
2020-09-10 20:53:48 -0400

    Updates

--- precache-manifest.c2f590cced1c39e5e29f21db64dccc91.js
+++ precache-manifest.c2f590cced1c39e5e29f21db64dccc91.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a1c01b787f621368370113e1ea1886c7",
+    "revision": "0c2a577ce796bd74aad0b5138fc0c969",
     "url": "/acnh-wtc/index.html"
   },
   {
-    "revision": "56e074274e9af9fb4a64",
-    "url": "/acnh-wtc/static/css/main.bde33bc4.chunk.css"
+    "revision": "a3d8d6c2bb3f1c0e50b4",
+    "url": "/acnh-wtc/static/css/main.888ebfa8.chunk.css"
   },
   {
     "revision": "52dc2cbb90a0ee44c4e7",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/acnh-wtc/static/js/2.b7fbd43e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "56e074274e9af9fb4a64",
+    "revision": "a3d8d6c2bb3f1c0e50b4",
     "url": "/acnh-wtc/static/js/main.61fce6bd.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/acnh-wtc/precache-manifest.e24d2e5f38b7fc345ed1304eb2d1eeab.js"
+  "/acnh-wtc/precache-manifest.c2f590cced1c39e5e29f21db64dccc91.js"
 );
 
 self.addEventListener('message', (event) => {
